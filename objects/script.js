881591aefc04f9d2b0418b71e10553fb9aa8756b
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2025-09-17 20:18:27 +0200

    Add files via upload

--- objects/script.js
+++ objects/script.js
@@ -21,38 +21,84 @@ do {
         "Odaberite opciju:\n" +
         "1. Upis novog pregleda\n" +
         "2. Pretraživanje postojećih pregleda\n" +
-        "3. Izlaz iz programa"
+        "3. Brisanje zakazanog pregleda\n" +
+        "4. Ispis svih pregleda na određeni datum\n" +
+        "0. Izlaz iz programa"
     );
 
-    if (commandInput === "1") {
-        const nameInput = prompt("Unesite ime i prezime za pretragu:");
-        const dateInput = prompt("Unesite datum pregleda za pretragu:");
-
-        const appointment = {
-            name: nameInput,
-            date: dateInput,
-        };
-
-        appointments.push(appointment);
-    } else if (commandInput === "2") {
-        const nameInput = prompt("Unesite ime i prezime za pretragu:");
-
-        for (let i = 0; i < appointments.length; i++) {
-            const appointment = appointments[i];
-
-            if (appointment.name === nameInput) {
-                alert(`Pregled za ${appointment.name} je zakazan za ${appointment.date}`);
-                break;
-            }
-
-            if (i === appointments.length - 1) {
-                alert("Nema zakazanih pregleda za uneseno ime i prezime");
-            }
-        }
-    } else if (commandInput === "3") {
-        // Izlaz
-    } else {
-        alert("Nepoznata opcija");
+    switch (commandInput) {
+        case "1": createAppointment(appointments); break;
+        case "2": findAppointment(appointments); break;
+        case "3": deleteAppointment(appointments); break;
+        case "4": listAppointmentsByDate(appointments); break;
+        case "0": break;
+        default: alert("Nepoznata opcija");
     }
-} while (commandInput !== "3");
+} while (commandInput !== "0");
 
+function createAppointment(appointments) {
+    const nameInput = prompt("Unesite ime i prezime za pretragu:");
+    const dateInput = prompt("Unesite datum pregleda za pretragu:");
+
+    const appointment = {
+        name: nameInput,
+        date: dateInput,
+    };
+
+    appointments.push(appointment);
+}
+
+function findAppointment(appointments) {
+    const nameInput = prompt("Unesite ime i prezime za pretragu:");
+
+    for (let i = 0; i < appointments.length; i++) {
+        const appointment = appointments[i];
+
+        if (appointment.name === nameInput) {
+            alert(`Pregled za ${appointment.name} je zakazan za ${appointment.date}`);
+            break;
+        }
+
+        if (i === appointments.length - 1) {
+            alert("Nema zakazanih pregleda za uneseno ime i prezime");
+        }
+    }
+}
+
+function deleteAppointment(appointments) {
+    const nameInput = prompt("Unesite ime i prezime za brisanje pregleda:");
+
+    let indexToDelete = -1;
+
+    for (let i = 0; i < appointments.length; i++) {
+        const appointment = appointments[i];
+
+        if (appointment.name === nameInput) {
+            indexToDelete = i;
+            break;
+        }
+    }
+
+    if (indexToDelete === -1) {
+        alert("Nema zakazanih pregleda za uneseno ime i prezime");
+
+        return;
+    }
+
+    appointments.splice(indexToDelete, 1);
+    alert(`Pregled za ${nameInput} je obrisan.`);
+}
+
+function listAppointmentsByDate(appointments) {
+    const dateInput = prompt("Unesite datum za ispis svih pregleda:");
+
+    const filteredAppointments = appointments.filter(function (appointment) {
+        return appointment.date === dateInput;
+    });
+
+    const names = filteredAppointments.map(function (appointment) {
+        return appointment.name;
+    });
+
+    alert(`Zakazani pregledi za ${dateInput}:\n` + names.join(", "));
+}
