a7c47b4709f603399da929f5e5364ed7484142ff
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2025-09-25 20:04:08 +0200

    Add files via upload

--- task/script.js
+++ task/script.js
@@ -2,10 +2,26 @@
     const form = document.querySelector("#form");
     const input = document.querySelector("#task");
     const output = document.querySelector("#output");
+    const submit = document.querySelector("#submit");
+
+    submit.setAttribute('disabled', 'disabled');
+
+    input.addEventListener("input", function () {
+        if (input.value.trim() !== '') {
+            submit.removeAttribute('disabled');
+        } else {
+            submit.setAttribute('disabled', 'disabled');
+        }
+    });
 
     form.addEventListener("submit", function (event) {
         event.preventDefault();
-        const task = input.value;
-        output.innerHTML += `<p>${task}</p>`;
+        const task = input.value.trim();
+
+        if (task !== '') {
+            output.innerHTML += `<p>${task}</p>`;
+            input.value = "";
+            submit.setAttribute('disabled', 'disabled');
+        }
     });
 })();
