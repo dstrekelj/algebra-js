9a7ca2b09c64ab2d3cb2806eaaeeceb1b03c12de
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2025-09-25 20:32:16 +0200

    Add files via upload

--- task/script.js
+++ task/script.js
@@ -1,4 +1,27 @@
+/**
+ * Napravite klasu TaskList koja:
+ * - se konstruira sa praznim nizom "tasks"
+ * - koja ima metodu addTask() koja
+ *  prima Task objekt kao parametar,
+ *  sprema ga u niz "tasks"
+ * - koja ima metodu render() koja
+ *  prima HTML element kao parametar,
+ *  briše njegov sadržaj,
+ *  ispisuje sve taskove iz "tasks"
+ * 
+ * Konstruirajte klasu TaskList i spremite u konstantu "taskList".
+ * 
+ * Na submit obrasca dodajte kreirani task u TaskList pomoću metode addTask()
+ * i pozovite metodu render() da ispišete sve taskove
+ */
+
 (function () {
+    class Task {
+        constructor(name) { this.name = name; }
+        toHtml() { return `<p>TASK: ${this.name}</p>`; }
+    }
+
+    const tasks = [];
     const form = document.querySelector("#form");
     const input = document.querySelector("#task");
     const output = document.querySelector("#output");
@@ -16,11 +39,16 @@
 
     form.addEventListener("submit", function (event) {
         event.preventDefault();
-        const task = input.value.trim();
+        const task = new Task(input.value.trim());
+        tasks.push(task);
 
-        if (task !== '') {
-            output.innerHTML += `<p>${task}</p>`;
-            input.value = "";
+        output.innerHTML = '';
+        for (let i = 0; i < tasks.length; i++) {
+            output.innerHTML += tasks[i].toHtml();
+        }
+
+        if (task.name !== '') {
+            input.value = '';
             submit.setAttribute('disabled', 'disabled');
         }
     });
