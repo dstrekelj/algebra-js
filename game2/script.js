c7416f8fa56d7ddce0d1c31ba5c1be5b7846f80b
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2025-10-01 21:04:38 +0200

    Add files via upload

--- game2/script.js
+++ game2/script.js
@@ -7,34 +7,64 @@
             this.width = width;
             this.height = height;
             this.color = color;
+            this.div = document.createElement('div');
         }
 
         update() { }
 
         render(element) {
-            const div = document.createElement('div');
-            div.style.position = 'fixed';
-            div.style.left = `${this.x}px`;
-            div.style.top = `${this.y}px`;
-            div.style.width = `${this.width}px`;
-            div.style.height = `${this.height}px`;
-            div.style.backgroundColor = this.color;
-            element.appendChild(div);
+            this.div.style.position = 'fixed';
+            this.div.style.left = `${this.x}px`;
+            this.div.style.top = `${this.y}px`;
+            this.div.style.width = `${this.width}px`;
+            this.div.style.height = `${this.height}px`;
+            this.div.style.backgroundColor = this.color;
+            element.appendChild(this.div);
         }
     }
 
     class Player extends Box {
         constructor(x, y) {
             super(x, y, 50, 50, 'blue');
+
+            window.addEventListener('keydown', (e) => {
+                switch (e.key) {
+                    case 'ArrowUp':
+                        this.y -= this.height;
+                        this.y = Math.max(this.y, 0);
+                        break;
+                    case 'ArrowDown':
+                        this.y += this.height;
+                        this.y = Math.min(this.y, window.innerHeight - this.height);
+                        break;
+                    case 'ArrowLeft':
+                        this.x -= this.width;
+                        this.x = Math.max(this.x, 0);
+                        break;
+                    case 'ArrowRight':
+                        this.x += this.width;
+                        this.x = Math.min(this.x, window.innerWidth - this.width);
+                        break;
+                    default:
+                        break;
+                }
+            });
         }
     }
 
     class Obstacle extends Box {
         constructor(x, y) {
-            super(x, y, 100, 100, 'red');
+            super(x, y, 50, 50, 'red');
+
+            this.sleepTime = 10;
         }
 
         update() {
+            if (this.sleepTime > 0) {
+                this.sleepTime -= 1;
+                return;
+            }
+
             const randomX = Math.random();
             const randomY = Math.random();
 
@@ -45,6 +75,8 @@
             this.x = Math.max(this.x, 0);
             this.y = Math.min(this.y, window.innerHeight - this.height);
             this.y = Math.max(this.y, 0);
+
+            this.sleepTime = 10;
         }
     }
 
@@ -59,5 +91,5 @@
         body.innerHTML = '';
         player.render(body);
         obstacle.render(body);
-    }, 1000 / 2);
+    }, 1000 / 30);
 })();
