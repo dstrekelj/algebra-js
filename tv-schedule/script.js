275ef9c6821fca33c286a1489e858d9454d807a2
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2025-10-13 19:28:27 +0200

    Add files via upload

--- tv-schedule/script.js
+++ tv-schedule/script.js
@@ -1,4 +1,44 @@
 (function () {
+    class Episode {
+        constructor(name, season, number, airstamp, showName) {
+            this.name = name;
+            this.season = season;
+            this.number = number;
+            this.airstamp = airstamp;
+            this.showName = showName;
+        }
+
+        isAiring(date) {
+            const episodeDate = new Date(this.airstamp);
+
+            return date.getDate() === episodeDate.getDate() &&
+                date.getMonth() === episodeDate.getMonth() &&
+                date.getFullYear() === episodeDate.getFullYear();
+        }
+
+        render() {
+            const div = document.createElement('div');
+
+            div.innerHTML = `
+                <strong>${this.showName}</strong><br>
+                S${this.season}E${this.number} ${this.name}<br>
+                ${this.airstamp}
+                <hr>
+            `;
+
+            return div;
+        }
+
+        static fromResponse(response) {
+            return new Episode(
+                response.name,
+                response.season,
+                response.number,
+                response.airstamp,
+                response._embedded.show.name
+            );
+        }
+    }
 
     class App {
         constructor() { }
@@ -7,28 +47,17 @@
             fetch('https://api.tvmaze.com/schedule/full')
                 .then(response => response.json())
                 .then(data => {
-                    /**
-                     * Filtrirati sve epizode tako da se prikažu samo one
-                     * koje se emitiraju danas (trenutni datum na klijentu).
-                     */
-                    data.filter(/* TU IDE FUNKCIJA ZA FILTER */).forEach(episode => {
-                        const episodeObject = {
-                            name: episode.name,
-                            season: episode.season,
-                            number: episode.number,
-                            airstamp: episode.airstamp,
-                            showName: episode._embedded.show.name,
-                        }
+                    data.map(episodeData => Episode.fromResponse(episodeData))
+                        .filter(episode => {
+                            const today = new Date();
 
-                        const div = document.createElement('div');
-                        div.innerHTML = `
-                            ${episodeObject.showName}<br>
-                            S${episodeObject.season}E${episodeObject.number} ${episodeObject.name}<br>
-                            ${episodeObject.airstamp}
-                            <hr>
-                        `;
-                        document.body.appendChild(div);
-                    });
+                            return episode.isAiring(today);
+                        })
+                        .forEach(episode => {
+                            const episodeElement = episode.render();
+
+                            document.body.appendChild(episodeElement);
+                        });
                 })
                 .catch(error => {
                     console.error(error);
