eea12aba20ba987c6637a8f67ce1867a02d19628
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2025-10-13 20:43:55 +0200

    Add files via upload

--- tv-schedule/script.js
+++ tv-schedule/script.js
@@ -1,11 +1,12 @@
 (function () {
     class Episode {
-        constructor(name, season, number, airstamp, showName) {
+        constructor(name, season, number, airstamp, showName, showSummary) {
             this.name = name;
             this.season = season;
             this.number = number;
             this.airstamp = airstamp;
             this.showName = showName;
+            this.showSummary = showSummary;
         }
 
         isAiring(date) {
@@ -26,6 +27,12 @@
                 <hr>
             `;
 
+            div.querySelector('strong').addEventListener('click', () => {
+                // prikažite negdje naslov i sažetak serije!
+                // korisniku morate omogućiti da i dalje vidi raspored i filtrira raspored
+                // možete koristiti CSS i HTML ali nemojte koristiti alert()
+            });
+
             return div;
         }
 
@@ -35,7 +42,8 @@
                 response.season,
                 response.number,
                 response.airstamp,
-                response._embedded.show.name
+                response._embedded.show.name,
+                response._embedded.show.summary
             );
         }
     }
@@ -44,38 +52,43 @@
         constructor(element) {
             this.element = element;
             this.episodes = [];
-        }
+            this.isReady = false;
 
-        renderSchedule(showName) {
-            this.element.innerHTML = '';
+            this.element.innerHTML = 'Loading...';
 
             fetch('https://api.tvmaze.com/schedule/full')
                 .then(response => response.json())
                 .then(data => {
+                    console.log(data);
                     this.episodes = data.map(episodeData => Episode.fromResponse(episodeData));
-
-                    this.episodes
-                        .filter(episode => {
-                            const today = new Date();
-
-                            if (!episode.isAiring(today)) {
-                                return false;
-                            }
-
-                            if (!showName) {
-                                return true;
-                            }
-
-                            return episode.showName.toLowerCase().includes(showName.toLowerCase());
-                        })
-                        .forEach(episode => {
-                            const episodeElement = episode.render();
-
-                            this.element.appendChild(episodeElement);
-                        });
+                    this.isReady = true;
+                    this.renderSchedule();
                 })
                 .catch(error => {
                     console.error(error);
+                });
+        }
+
+        renderSchedule(showName) {
+            if (!this.isReady) {
+                return;
+            }
+
+            this.element.innerHTML = '';
+
+            this.episodes
+                .filter(episode => {
+                    const today = new Date();
+
+                    if (!episode.isAiring(today)) return false;
+                    if (!showName) return true;
+
+                    return episode.showName.toLowerCase().includes(showName.toLowerCase());
+                })
+                .forEach(episode => {
+                    const episodeElement = episode.render();
+
+                    this.element.appendChild(episodeElement);
                 });
         }
     }
